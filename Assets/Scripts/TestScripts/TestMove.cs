b229a4efc5029312d5191bae1f801587aed0a8bf
kennyyyyyy <15973972093@163.com>
2021-03-28 21:44:36 +0800

    添加动画状态机

--- Assets/Scripts/TestScripts/TestMove.cs
+++ Assets/Scripts/TestScripts/TestMove.cs
@@ -5,6 +5,8 @@ using UnityEngine.Tilemaps;
 
 public class TestMove : MonoBehaviour
 {
+    public Animator animator;
+
     public Rigidbody2D rb;
     public float force;
     public float MaxVelocity;
@@ -14,20 +16,23 @@ public class TestMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        vertical = 1.0f;
         if (rb == null)
             rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    //获得左右转向来控制动画的播放
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        if(Input.GetAxis("Vertical") < 0)
+            vertical = 1.0f + Input.GetAxis("Vertical");
+        animator.SetFloat("Horizontal", horizontal);
     }
 
-    private void FixedUpdate()
-    {
-        rb.AddForce(new Vector2(horizontal, vertical) * force, ForceMode2D.Impulse);
-        rb.velocity = Vector2.ClampMagnitude(rb.velocity, MaxVelocity);
-    }
+    //private void FixedUpdate()
+    //{
+    //    rb.AddForce(new Vector2(horizontal, vertical) * force, ForceMode2D.Impulse);
+    //    rb.velocity = Vector2.ClampMagnitude(rb.velocity, MaxVelocity);
+    //}
 }
